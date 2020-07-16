a85a518e7593f6b2618ed534c06c37d4bd9f34d2
Steven Harris <stevenharrisdev@gmail.com>
2020-07-15 22:28:59 -0500

    created example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ContentLoaderModule } from '@ngneat/content-loader';
 
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ContentLoaderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
